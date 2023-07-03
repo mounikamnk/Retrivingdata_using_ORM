f862446acd5d0a3a3360cd78b355bd69e23481e3
mounikamnk <nasanammounika@gmail.com>
2023-07-03 10:12:00 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from app.models import *
 from django.db.models.functions import Length
 from django.db.models import Q
+from django.http import HttpResponse
 # Create your views here.
 def display_topics(request):
     LTO=Topic.objects.all()
@@ -32,5 +33,39 @@ def display_access(request):
     LAO=AccessRecord.objects.filter(Q(name='muni')|Q(author='abcd'))
     LAO=AccessRecord.objects.filter(Q(name='muni')&Q(author='abcd'))
     LAO=AccessRecord.objects.all()
+    LAO=AccessRecord.objects.filter(date__month='07')
+    LAO=AccessRecord.objects.filter(date__day='13')
+    LAO=AccessRecord.objects.filter(author__regex='m\w+')
+    LAO=AccessRecord.objects.filter(author__in=['muni','abc'])
+    LAO=AccessRecord.objects.filter(author__startswith='m')
+    LAO=AccessRecord.objects.filter(author__endswith='d')
+    LAO=AccessRecord.objects.filter(author__contains='a')
+    LAO=AccessRecord.objects.filter(date__year__gt='2021')
+
     d={'LAO':LAO}
     return render(request,'display_access.html',d)
+
+
+def update_webpages(request):
+    # Webpage.objects.filter(name='dhoni').update(url='http://msd.com')
+    # Webpage.objects.filter(topic_name='cricket').update(url='http://indiateam.com')
+    # Webpage.objects.filter(name='alekhya').update(url='http://alekhya.com')
+    # Webpage.objects.filter(name='dhoni').update(topic_name='hockey')
+    # Webpage.objects.filter(name='dhoni').update(topic_name='rugby')
+
+
+    #Webpage.objects.update_or_create(name='dhoni',defaults={'url':'http://abc.com'})
+    # Webpage.objects.update_or_create(topic_name='rugby',defaults={'url':'http://rugby.com'})
+    cto=Topic.objects.get(topic_name='cricket')
+    Webpage.objects.update_or_create(name='abc',defaults={'topic_name':cto})
+    Webpage.objects.update_or_create(name='mounika',defaults={'topic_name':cto,'url':'http://mouni.com'})
+    LWO=Webpage.objects.all()
+    d={'LWO':LWO}
+    return render(request,'display_webpages.html',d)
+def delete_webpages(request):
+    Webpage.objects.filter(name='virat').delete()
+    Webpage.objects.filter(topic_name='cricket').delete()
+
+    LWO=Webpage.objects.all()
+    d={'LWO':LWO}
+    return render(request,'display_webpages.html',d)
--- project33/urls.py
+++ project33/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('display_topics/',display_topics,name='display_topics'),
     path('display_webpages/',display_webpages,name='display_webpages'),
     path('display_access/',display_access,name='display_access'),
+    path('update_webpages/',update_webpages,name='update_webpages'),
+    path('delete_webpages/',delete_webpages,name='delete_webpages'),
 ]
